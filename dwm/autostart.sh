0353b14a9edfd65d269eff27cb40aec4aae658fe
pankx <a3427173515@163.com>
2023-05-22 20:52:45 +0800

    添加 dwm 自启动搅拌

---
+++ dwm/autostart.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+#while true;
+#do
+#	feh --bg-fill --randomize --no-fehbg ~/wm/wallpaper/*.png
+#	sleep 6m
+#done &
+
+
+feh --bg-fill --randomize --no-fehbg ~/wm/wallpaper/01.png &
+
+fcitx5 &
+
+# dunst &
