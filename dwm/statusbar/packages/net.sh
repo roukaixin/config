88a7bcbebf532c93b2edef6a4fb4dc9e69dc5278
pankx <pankaixin@kamfu.com.cn>
2023-06-07 10:55:13 +0800

    发生通知的文字动态

--- dwm/statusbar/packages/net.sh
+++ dwm/statusbar/packages/net.sh
@@ -17,11 +17,13 @@ then
   wifi_disconnected="disconnected"
   wifi_disconnected_notify="disconnected"
   wifi_delimiter=":  "
+  internet_name="Internet"
 else
   wifi_grep_keyword="已连接 到"
   wifi_disconnected="未连接"
   wifi_disconnected_notify="未连接到网络"
   wifi_delimiter="： "
+  internet_name="以太网"
 fi
 
 update() {
@@ -29,8 +31,10 @@ update() {
   connection_method=$(nmcli | grep "$wifi_grep_keyword" | sed "s/$wifi_grep_keyword//" | awk -F ":  " '{print $1}' | awk -F '' '{print $1$2}')
   if [ "$connection_method" == "en" ]; then
     net_icon="󰈀"
+    connection_name=$internet_name
   else
     net_icon="󰤨"
+    connection_name="Wifi"
   fi
 
   net_text=$(nmcli | grep "$wifi_grep_keyword" | sed "s/$wifi_grep_keyword//" | awk -F "$wifi_delimiter" '{print $2}' | paste -d " " -s)
@@ -45,7 +49,7 @@ update() {
 
 notify() {
     update
-    dunstify -r 9527 "$net_icon Wifi" "\n$net_text"
+    dunstify -r 9527 "$net_icon $connection_name" "\n$net_text"
 }
 
 call_nm() {
