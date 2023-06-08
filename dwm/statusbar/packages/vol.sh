5a14df94b61c52418ef7c6f4868dde5946aa6ef3
pankx <a3427173515@163.com>
2023-06-08 21:32:12 +0800

    中英文适配，代码规范修改

--- dwm/statusbar/packages/vol.sh
+++ dwm/statusbar/packages/vol.sh
@@ -14,7 +14,8 @@
 # 静音 -> Mute: no                                                                                 
 # 音量 -> Volume: front-left: 13183 /  20% / -41.79 dB,   front-right: 13183 /  20% / -41.79 dB
 
-tempfile=$(cd $(dirname $0);cd ..;pwd)/temp
+# shellcheck disable=SC2046
+temp_file=$(cd $(dirname "$0") || exit;cd ..;pwd)/temp
 
 this=_vol
 icon_color="^c#442266^^b#7879560x88^"
@@ -25,25 +26,31 @@ signal=$(echo "^s$this^" | sed 's/_//')
 [ ! "$(command -v pactl)" ] && echo command not found: pactl && exit
 
 update() {
-    sink=$(pactl info | grep 'Default Sink' | awk '{print $3}')
-    if [ "$sink" = "" ]; then sink=$(pactl info | grep '默认音频入口' | awk -F'：' '{print $2}');fi
-    volunmuted=$(pactl list sinks | grep $sink -A 6 | sed -n '7p' | grep '静音：否')
-    vol_text=$(pactl list sinks | grep $sink -A 7 | sed -n '8p' | awk '{printf int($4)}')
-    if [ "$LANG" != "zh_CN.UTF-8" ]; then
-        volunmuted=$(pactl list sinks | grep $sink -A 6 | sed -n '7p' | grep 'Mute: no')
-        vol_text=$(pactl list sinks | grep $sink -A 7 | sed -n '8p' | awk '{printf int($5)}')
-    fi
-    if [ ! "$volunmuted" ];      then vol_text="--"; vol_icon="󰸈";
-    elif [ "$vol_text" -eq 0 ];  then vol_text="00"; vol_icon="󰕿";
-    elif [ "$vol_text" -lt 10 ]; then vol_icon=""; vol_text=0$vol_text;
-    elif [ "$vol_text" -le 50 ]; then vol_icon="󱄠";
-    else vol_icon=""; fi
+  sink=$(pactl info | grep 'Default Sink' | awk '{print $3}')
+  vol_text=$(pactl list sinks | grep "$sink" -A 7 | sed -n '8p' | awk -F "/" '{printf int($2)}')
+  vol_toggle_cn=$(pactl list sinks | grep "$sink" -A 6 | sed -n '7p' | grep '静音:' | awk -F ": " '{print $2}')
+  vol_toggle_us=$(pactl list sinks | grep "$sink" -A 6 | sed -n '7p' | grep 'Mute:' | awk -F ": " '{print $2}')
+  if [ "$vol_toggle_us" == "yes" ] || [ "$vol_toggle_cn" == "是" ];
+  then
+    vol_text="--"; vol_icon="󰸈";
+  elif [ "$vol_text" -eq 0 ];
+  then
+    vol_text="00"; vol_icon="󰕿";
+  elif [ "$vol_text" -lt 10 ];
+  then
+    vol_icon=""; vol_text=0$vol_text;
+  elif [ "$vol_text" -le 50 ];
+  then
+    vol_icon="󱄠";
+  else
+    vol_icon="";
+  fi
 
-    icon=" $vol_icon "
-    text=" $vol_text% "
+  icon=" $vol_icon "
+  text=" $vol_text% "
 
-    sed -i '/^export '$this'=.*$/d' $tempfile
-    printf "export %s='%s%s%s%s%s'\n" $this "$signal" "$icon_color" "$icon" "$text_color" "$text" >> $tempfile
+  sed -i '/^export '$this'=.*$/d' "$temp_file"
+  printf "export %s='%s%s%s%s%s'\n" $this "$signal" "$icon_color" "$icon" "$text_color" "$text" >> "$temp_file"
 }
 
 notify() {
@@ -80,7 +87,7 @@ click() {
 }
 
 case "$1" in
-    click) click $2 ;;
+    click) click "$2" ;;
     notify) notify ;;
     *) update ;;
 esac
