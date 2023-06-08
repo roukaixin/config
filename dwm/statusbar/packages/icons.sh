fdbc06650fbb9be6239c806192651b1093e5a59e
pankx <a3427173515@163.com>
2023-06-09 07:57:03 +0800

    icons 脚本码规范修改，格式修改，变量命名

--- dwm/statusbar/packages/icons.sh
+++ dwm/statusbar/packages/icons.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 # ICONS 部分特殊的标记图标 这里是我自己用的，你用不上的话去掉就行
 
-tempfile=$(cd $(dirname $0);cd ..;pwd)/temp
+shell_path=$(dirname "$0")
+temp_file=$(cd "$shell_path" || exit;cd ..;pwd)/temp
 
 this=_icons
 color="^c#2D1B46^^b#5555660x66^"
 signal=$(echo "^s$this^" | sed 's/_//')
 
 update() {
-    icons=("󰣇")
+  icons=("󰣇")
 
-    text=" ${icons[@]} "
+  text=" ${icons[*]} "
 
-    sed -i '/^export '$this'=.*$/d' $tempfile
-    printf "export %s='%s%s%s'\n" $this "$signal" "$color" "$text" >> $tempfile
+  sed -i '/^export '$this'=.*$/d' "$temp_file"
+  printf "export %s='%s%s%s'\n" $this "$signal" "$color" "$text" >> "$temp_file"
 }
 
 call_menu() {
@@ -21,18 +22,18 @@ call_menu() {
         " 关机") poweroff ;;
         " 重启") reboot ;;
         " 休眠") systemctl hibernate ;;
-        " 锁定") ~/scripts/blurlock.sh ;;
+        " 锁定") ~/wm/config/lock/blurlock.sh ;;
     esac
 }
 
 click() {
     case "$1" in
-        L)  feh --bg-fill --randomize --no-fehbg ~/wm/wallpaper/*.png ;;
+        L) feh --bg-fill --randomize --no-fehbg ~/wm/wallpaper/*.png ;;
         R) call_menu ;;
     esac
 }
 
 case "$1" in
-    click) click $2 ;;
+    click) click "$2" ;;
     *) update ;;
 esac
