7cace57bf30267d263f50a3fa9fda86113534199
pankx <a3427173515@163.com>
2024-02-23 22:21:18 +0800

    refactor: 修改自启动脚本

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,21 +1,19 @@
 #! /bin/bash
 
-
-
 daemons() {
-  sh "$HOME"/wm/config/dwm/statusbar/statusbar.sh cron &
   fcitx5 &
-  flameshot &
-  dunst -conf "$HOME"/wm/config/dunst/dunstrc &
-  picom --config "$HOME"/wm/config/picom/picom.conf &
+  numlockx &
   nm-applet &
+  blueman-applet &
+  /usr/lib/polkit-kde-authentication-agent-1 &
+  picom -b
 }
 
 cron() {
 	while true;
 	do
 		feh --bg-fill --randomize --no-fehbg "$HOME"/wm/wallpaper/*.png
-		sleep 300
+		sleep 1800
 	done &
 }
 
